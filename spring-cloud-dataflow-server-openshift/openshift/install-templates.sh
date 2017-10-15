33b34fa7b5df055ed90b310e4a2da8d4a8194414
Ravindra Kumar Bhatt <ravindra.bhatt29@gmail.com>
2017-10-15 15:13:48 +0530

    Update openshift deployment scripts

---
+++ spring-cloud-dataflow-server-openshift/openshift/install-templates.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+project=${1:-scdf}
+
+echo "Installing OpenShift templates into project '${project}'..."
+
+
+oc replace --force=true -f ./deployment-config.yaml
+
+echo "Adding 'edit' role to 'scdf' Service Account..."
+
+oc policy add-role-to-user edit system:serviceaccount:${project}:scdf
+
+echo "Adding 'scdf' Service Account to the 'anyuid' SCC..."
+
+oc adm policy add-scc-to-user anyuid system:serviceaccount:scdf:scdf
+
+echo "Templates installed."
--- spring-cloud-dataflow-server-openshift/openshift/pull-images.sh
+++ spring-cloud-dataflow-server-openshift/openshift/pull-images.sh
@@ -3,10 +3,7 @@
 echo "Pulling images..."
 
 declare -a images=(
-  "mysql:5.6"
-  "redis:3-alpine"
   "donovanmuller/spring-cloud-dataflow-server-openshift:1.1.0.RELEASE"
-  "rabbitmq:3-management"
   "digitalwonderland/zookeeper"
   "wurstmeister/kafka:0.10.1.0"
   )
