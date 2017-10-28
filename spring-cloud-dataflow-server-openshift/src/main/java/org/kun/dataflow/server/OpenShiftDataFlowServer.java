fb3f1a3f5ced572d5be9b8f1cdbd919ac4cda067
Ravindra Kumar Bhatt <ravindra.bhatt29@gmail.com>
2017-10-28 08:32:43 +0530

    Change package name

--- spring-cloud-dataflow-server-openshift/src/main/java/org/kun/dataflow/server/OpenShiftDataFlowServer.java
+++ spring-cloud-dataflow-server-openshift/src/main/java/org/kun/dataflow/server/OpenShiftDataFlowServer.java
@@ -1,4 +1,4 @@
-package org.springframework.cloud.dataflow.server.openshift;
+package org.kun.dataflow.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
