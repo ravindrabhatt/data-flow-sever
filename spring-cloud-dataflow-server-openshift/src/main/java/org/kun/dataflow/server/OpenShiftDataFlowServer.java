2bf4df73ad82777fec0de317850fa86e58557036
Ravindra Kumar Bhatt <ravindra.bhatt29@gmail.com>
2017-11-21 14:48:19 +0530

    Disable kubernetes auto configuration by default

--- spring-cloud-dataflow-server-openshift/src/main/java/org/kun/dataflow/server/OpenShiftDataFlowServer.java
+++ spring-cloud-dataflow-server-openshift/src/main/java/org/kun/dataflow/server/OpenShiftDataFlowServer.java
@@ -3,13 +3,14 @@ package org.kun.dataflow.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.deployer.spi.kubernetes.KubernetesAutoConfiguration;
 
 /**
  * Bootstrap class for the OpenShift 3 Spring Cloud Data Flow Server.
  *
  * @author Donovan Muller
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = KubernetesAutoConfiguration.class)
 @EnableDataFlowServer
 public class OpenShiftDataFlowServer {
 
